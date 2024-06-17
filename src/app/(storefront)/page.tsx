4b0a391766f9077f72ffd955c27c4743dc400d9f
imranDev20 <imrandev20@gmail.com>
2024-06-17 22:30:45 +0600

    Add prisma and postgresql

--- src/app/(storefront)/page.tsx
+++ src/app/(storefront)/page.tsx
@@ -1,4 +1,22 @@
-export default function HomePage() {
+import prisma from "@/lib/prisma";
+
+async function getPosts() {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+
+  return feed;
+}
+
+export default async function HomePage() {
+  const posts = await getPosts();
+  console.log(posts);
+
   return (
     <div className="flex flex-col min-h-screen">
       <main className="min-h-[calc(100vh-57px-97px)] flex-1">
