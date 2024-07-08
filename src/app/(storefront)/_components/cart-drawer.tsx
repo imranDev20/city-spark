b5c0fd5159526cf38c234a78c99d31242b2a846c
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2024-07-08 17:19:45 +0600

    done implementing "Cart Page for the storefront"

--- src/app/(storefront)/_components/cart-drawer.tsx
+++ src/app/(storefront)/_components/cart-drawer.tsx
@@ -1,6 +1,15 @@
+import { cartData } from "@/app/data";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
   SheetClose,
@@ -11,7 +20,9 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { ShoppingCartIcon } from "lucide-react";
+
+import { CircleMinus, CirclePlus, ShoppingCartIcon, X } from "lucide-react";
+import Link from "next/link";
 
 export default function CartDrawer() {
   return (
@@ -24,35 +35,59 @@ export default function CartDrawer() {
           <ShoppingCartIcon className="text-primary" />
           <div className="flex flex-col items-start ml-2">
             <p className="text-xs text-neutral-500 ">Basket</p>
-            <p className="text-sm">0 items</p>
+            <p className="text-sm">{cartData.length} items</p>
           </div>
         </Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&lsquo;re
-            done.
-          </SheetDescription>
+          <SheetTitle>Items</SheetTitle>
+          <Separator />
         </SheetHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+          {cartData.map((data) => {
+            return (
+              <>
+                <div className="grid grid-cols-[5%_35%_55%_5%] gap-1">
+                  <div className="flex flex-col justify-center">
+                    <button type="button">
+                      <CirclePlus className="text-primary" />
+                    </button>
+                    <span className="ms-2">1</span>
+                    <button type="button">
+                      <CircleMinus className="text-primary" />
+                    </button>
+                  </div>
+                  <div className="ms-4">
+                    <img
+                      src={data?.src}
+                      alt={data?.name}
+                      className="w-full h-20 "
+                    />
+                  </div>
+                  <div className="flex flex-col ms-2 gap-1">
+                    <p className="font-bold">{data?.name}</p>
+                    <small>
+                      <span className="text-secondary">${data?.price}</span> X{" "}
+                      <span>1</span>
+                    </small>
+                    <p className="text-secondary font-bold">${data?.price}</p>
+                  </div>
+                  <div className="flex  justify-end items-center">
+                    <X />
+                  </div>
+                </div>
+                <Separator />
+              </>
+            );
+          })}
         </div>
         <SheetFooter>
           <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
+            <Link className="flex content-center" href="/cart">
+              {" "}
+              <Button type="button">View Cart</Button>
+            </Link>
           </SheetClose>
         </SheetFooter>
       </SheetContent>
