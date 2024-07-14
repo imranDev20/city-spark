99008a78db034fd5c3fd6ee12ae4544a6d1e0986
Rahat393 <rahataminulislam37@gmail.com>
2024-07-14 12:36:01 +0600

    brand and template create, get, delete implement

---
+++ src/app/admin/brands/_components/brand-table-row.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { useToast } from "@/components/ui/use-toast";
+import { Prisma } from "@prisma/client";
+import { MoreHorizontal } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { deleteBrand } from "../actions";
+type BrandeWithRelations = Prisma.BrandGetPayload<{
+  include: {};
+}>;
+export default function BrandTableRow({
+  brand,
+}: {
+  brand: BrandeWithRelations;
+}) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const { toast } = useToast();
+
+  const handleDelete = () => {
+    startTransition(async () => {
+      const result = await deleteBrand(brand.id);
+
+      if (result.success) {
+        console.log(result.message);
+
+        toast({
+          title: "Brand Deleted",
+          description: "The brand has been successfully deleted.",
+          variant: "destructive",
+        });
+      } else {
+        console.error(result.message);
+      }
+    });
+  };
+  return (
+    <TableRow
+      key={brand.id}
+      onClick={() => router.push(`/admin/brands/${brand.id}`)}
+      className={`cursor-pointer ${isPending ? "opacity-30" : "opacity-100"}`}
+    >
+      <TableCell className="hidden sm:table-cell">
+        <Image
+          alt="Product image"
+          className="aspect-square rounded-md object-cover"
+          height="64"
+          src="/placeholder.svg"
+          width="64"
+        />
+      </TableCell>
+      <TableCell className="font-medium">{brand.name}</TableCell>
+      <TableCell>
+        <Badge variant="outline">Draft</Badge>
+      </TableCell>
+      <TableCell className="hidden md:table-cell">$499.99</TableCell>
+      <TableCell className="hidden md:table-cell">25</TableCell>
+      <TableCell className="hidden md:table-cell">
+        2023-07-12 10:42 AM
+      </TableCell>
+      <TableCell>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button aria-haspopup="true" size="icon" variant="ghost">
+              <MoreHorizontal className="h-4 w-4" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={(e) => {
+                e.stopPropagation();
+                handleDelete();
+              }}
+            >
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
+    </TableRow>
+  );
+}
