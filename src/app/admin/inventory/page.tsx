4ed5ee2b207651751ded56518787604f15058c05
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2024-07-14 18:33:58 +0600

    make changes on inventory UI

--- src/app/admin/inventory/page.tsx
+++ src/app/admin/inventory/page.tsx
@@ -1,7 +1,9 @@
+import { Suspense } from "react";
 import { ContentLayout } from "../_components/content-layout";
 import DynamicBreadcrumb from "../_components/dynamic-breadcrumb";
 import InventoryList from "./_components/inventory-list";
 import { InventoryPagination } from "./_components/inventory-pagination";
+import InventoryTableHeader from "./_components/inventory-table-header";
 
 const breadcrumbItems = [
   { label: "Dashboard", href: "/admin" },
@@ -12,7 +14,10 @@ export default function InventoryPage() {
   return (
     <ContentLayout title="Inventory">
       <DynamicBreadcrumb items={breadcrumbItems} />
+      <InventoryTableHeader />
+      <Suspense fallback="Loading...">
       <InventoryList />
+      </Suspense>
       <InventoryPagination />
     </ContentLayout>
   );
