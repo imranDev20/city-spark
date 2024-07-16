ca63b96c45b315cff20c659bc5546b2ff08c588c
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2024-07-16 13:25:25 +0600

    working on inventory create page ui

--- src/app/admin/inventory/schema.ts
+++ src/app/admin/inventory/schema.ts
@@ -8,6 +8,9 @@ productId: z.string().min(1,'Select product is requied'),
 deliveryMinimumCount: z.number().optional(),
 deliveryMaximumCount: z.number().optional(),
 estimatedDeliveryTime: z.number().optional(),
+collectionMinimumCount: z.number().optional(),
+collectionMaximumCount: z.number().optional(),
+estimatedcollectionTime: z.number().optional(),
 
 
 });
