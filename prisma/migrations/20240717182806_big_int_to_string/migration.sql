ed88fcb41171a8f7c0ee9517c98dc2a9cda1743e
imranDev20 <imrandev20@gmail.com>
2024-07-29 15:12:34 +0600

    Revert scripts changes

--- prisma/migrations/20240717182806_big_int_to_string/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `size` on the `Image` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "Image" ALTER COLUMN "lastModified" SET DATA TYPE TEXT,
-ALTER COLUMN "size" SET DATA TYPE INTEGER;
