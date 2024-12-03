f52f48ca12464896142139f70d66a41f4103407f
Spiderbyte IT <itspiderbyte@gmail.com>
2024-12-03 18:27:33 +0600

    Fix homepage images sizes prop

--- src/app/(storefront)/_components/advertisement-two-column.tsx
+++ src/app/(storefront)/_components/advertisement-two-column.tsx
@@ -1,6 +1,5 @@
 import IdealAdlantic30CombiFlue from "@/images/advertisements/ideal-atlantic-30-combi-flue.jpg";
 import VaillantEcoFitPlus832Combi from "@/images/advertisements/vaillant-ecofit-plus-832-combi.jpg";
-
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
@@ -8,27 +7,31 @@ export default function AdvertisementTwoColumn() {
   return (
     <section
       className={cn(
-        "container max-w-screen-xl mx-auto my-12",
-        "px-4 md:px-6 lg:px-8",
-        "grid grid-cols-2 gap-5"
+        "container max-w-screen-xl mx-auto my-6 sm:my-8 lg:my-12",
+        "px-4 sm:px-6 lg:px-8",
+        "grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-5 lg:gap-8"
       )}
     >
-      <div className="lg:col-span-1 relative">
+      <div className="relative w-full ">
         <Image
           src={VaillantEcoFitPlus832Combi}
-          alt="Ideal Atlantic"
-          width={600}
-          height={400}
-          className="rounded-lg h-full"
+          alt="Vaillant EcoFit Plus 832 Combi Boiler"
+          width={800}
+          height={450}
+          className="rounded-lg object-cover object-center"
+          placeholder="blur"
+          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 600px"
         />
       </div>
-      <div className="lg:col-span-1 relative">
+      <div className="relative w-full">
         <Image
           src={IdealAdlantic30CombiFlue}
-          alt="Ideal Atlantic"
-          width={600}
-          height={400}
-          className="rounded-lg h-full"
+          alt="Ideal Atlantic 30 Combi Flue Boiler"
+          width={800}
+          height={450}
+          className="rounded-lg object-cover object-center"
+          placeholder="blur"
+          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 600px"
         />
       </div>
     </section>
