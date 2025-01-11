4c05c904b0a8ec66ff96deee44686d92714b99b0
Spiderbyte IT <itspiderbyte@gmail.com>
2025-01-12 01:43:35 +0600

    Add suspense in login

--- src/app/(storefront)/login/page.tsx
+++ src/app/(storefront)/login/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import LoginForm from "./_components/login-form";
 
 export default function LoginPage() {
   return (
-    <>
+    <Suspense fallback="Loading...">
       <LoginForm />
-    </>
+    </Suspense>
   );
 }
