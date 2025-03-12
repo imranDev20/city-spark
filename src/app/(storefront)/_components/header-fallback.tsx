7bc118d406cd0268053a2a92f111d1503b58e6e8
Imran Kabir <imrandev20@gmail.com>
2025-03-13 03:25:06 +0600

    Remove mobile header fallback

--- src/app/(storefront)/_components/header-fallback.tsx
+++ src/app/(storefront)/_components/header-fallback.tsx
@@ -11,7 +11,7 @@ export default function HeaderFallback() {
   return (
     <>
       {!isCheckoutPage && <DesktopHeaderFallback />}
-      <MobileHeader />
+      {/* <MobileHeader /> */}
     </>
   );
 }
